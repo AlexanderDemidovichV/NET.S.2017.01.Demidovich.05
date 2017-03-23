28565091ff3f35e400213cdb992727aea7c53992
Alexander <alex.dem.v@gmail.com>
2017-03-23 11:10:57 +0300

    add summary for Task2

--- Task2/NumberConverter.cs
+++ Task2/NumberConverter.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+    /// <summary>
+    /// Provides methods for number conversion
+    /// </summary>
     public static class NumberConverter
     {
+        #region Public Methods
+
+        /// <summary>
+        /// Returns the specified double-precision floating point value as a string representation of bits.
+        /// </summary>
+        /// <param name="value">The number to convert.</param>
+        /// <returns>A string representation of bits with length 64.</returns>
         public static string DoubleToBinary(double value)
         {
             var long64bits = BitConverter.ToInt64(BitConverter.GetBytes(value), 0);
@@ -15,11 +25,12 @@ namespace Task2
             var exponent64 = long64bits >> 52 & 0x7FF;
             var fraction64 = long64bits & 0xFFFFFFFFFFFFF;
 
-            var raw_str = Convert.ToString(long64bits, 2).PadLeft(64, '0');
-
             return String.Format($"{Convert.ToString(sign64, 2)}" +
                                  $"{Convert.ToString(exponent64, 2).PadLeft(11, '0')}" +
                                  $"{Convert.ToString(fraction64, 2).PadLeft(52, '0')}");
         }
+
+        #endregion
+
     }
 }
